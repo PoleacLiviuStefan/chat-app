380369cc8fdf8cb9603ccf345f33eb5e28fdf11a
Poleac Liviu Stefan <stefan.liviu286@gmail.com>
2024-06-12 00:53:34 +0300

    Added friends and chats on the homepage

---
+++ AplicatieMDS/Views/Home/_MessageFormPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AplicatieMDS.Views.Home
+{
+    public class _MessageFormPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
